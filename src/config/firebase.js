4a8decad62c5bb8a7e1520308428585c9e45aedf
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-19 08:16:01 -0300

    chore: add environment variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
+import env from 'react-dotenv'
 import { initializeApp } from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBiFNwr3c2UIodIO8FlIP4NxUN-Ps2axvc",
-    authDomain: "foodcalories-d53c8.firebaseapp.com",
-    projectId: "foodcalories-d53c8",
-    storageBucket: "foodcalories-d53c8.appspot.com",
-    messagingSenderId: "691018985799",
-    appId: "1:691018985799:web:63fb483242feb4a5761c62"
+    apiKey: env.FIREBASE_APIKEY,
+    authDomain: env.FIREBASE_AUTH_DOMAIN,
+    projectId: env.FIREBASE_CLIENTID,
+    storageBucket: env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: env.FIREBASE_MESSAGE_SENDER_ID,
+    appId: env.FIREBASE_APPID
 }
 
 // Initialize Firebase
