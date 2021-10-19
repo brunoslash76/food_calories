ee262b962ae7dd8ae409af11d423e3673265b2de
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-19 08:51:53 -0300

    feat: add global styles and theming

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components'
 import App from './App';
+import theme from './styles/theme.styles'
+import GlobalStyles from './styles/globalStyles.styles'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
