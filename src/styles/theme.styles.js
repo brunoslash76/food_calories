ac0a280797ded68480f7654c3f2e4da43b86d60a
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-19 08:51:40 -0300

    chore: add theming

---
+++ src/styles/theme.styles.js
@@ -0,0 +1,28 @@
+const theme = {
+    colors: {
+        primary: '#439CC2',
+        secundary: '#555555',
+        background: '#B7E1EE',
+        white: '#fefefe',
+        default: '#676767',
+        warning: '#ECBD77',
+        danger: '#B86D6D',
+    },
+    fonts: {
+        family: 'Roboto sans-serif',
+        sizes: {
+            xxlarge: '2.8rem',
+            xlarge: '2.0rem',
+            medium: '1.8rem',
+            small: '1.6rem',
+            xsmall: '1.4rem',
+            xxsmal: '1.3rem'
+        },
+        weight: {
+            thin: '100',
+            light: '300',
+            regular: '400',
+            bold: '700'
+        }
+    }
+}
