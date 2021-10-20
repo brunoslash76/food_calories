bf40bdddce42739feba470f8baf895d013087fd0
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-20 09:58:39 -0300

    feat: add globalStyles for anchor and paragraphs

--- src/styles/globalStyles.styles.js
+++ src/styles/globalStyles.styles.js
@@ -7,10 +7,16 @@ const GlobalStyles = createGlobalStyle`
         border: none;
         margin: 0;
         padding: 0;
+        
     }
 
     h1, h2, h3, h4, h5, h6, p {
         margin: 0;
+        font-family: ${({ theme }) => theme.fonts.family};
+    }
+
+    a, p {
+        font-size: 1.4rem;
     }
 
     ol, ul {
