187e1470b1a73d6d8ea21ff29bbe38b8b7db8ac3
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-20 10:04:36 -0300

    chore: add Firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import env from 'react-dotenv'
 import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth } from 'firebase/auth'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,4 +19,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-export default app
+const db = getFirestore(app)
+const auth = getAuth(app)
+export {
+    auth,
+    db
+}
