20d131aec458c3158df71bf73981e5d34c649453
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-22 16:08:04 -0300

    feat: add signOut to avatar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,16 +1,24 @@
+import { useDispatch } from 'react-redux'
+import { signOut } from '@firebase/auth'
 import Avatar from '@mui/material/Avatar'
-import Title from '../Title'
+import { auth } from '../../config/firebase'
+import { LOGOFF_USER } from '../../store/constants'
+import Title from '../Typography/Title'
 import { NavContainer } from './styles'
 
 const NavBar = ({ title }) => {
-    const user = {
-        name: 'Bruno Russo',
-        uid: '1234567890'
+    const dispatch = useDispatch()
+    const logoff = () => {
+        signOut(auth).then(() => {
+            localStorage.clear()
+            dispatch({ type: LOGOFF_USER })
+        })
     }
+
     return (
         <NavContainer>
-            <Title>{title}</Title>
-            <Avatar user={user} size='large' />
+            <Title color="white">{title}</Title>
+            <Avatar size='large' onClick={logoff} style={{ cursor: 'pointer' }} />
         </NavContainer>
     )
 }
