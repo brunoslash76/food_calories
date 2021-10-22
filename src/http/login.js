5ec827818f7b42e16648c34d404b1fc9903c2735
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-22 16:01:32 -0300

    feat: add login logics to firebase

--- src/http/login.js
+++ src/http/login.js
@@ -1,12 +1,29 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from '../config/firebase'
 
 export const login = async (email, password) => {
-    const auth = getAuth()
     try {
-        const user = await signInWithEmailAndPassword(auth, email, password)
-        return user
+        const res = await signInWithEmailAndPassword(auth, email, password)
+        const docRef = doc(db, 'users', res.user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            return {
+                error: false,
+                data: docSnap.data()
+            }
+        } else {
+            return {
+                error: true,
+                data: null
+            }
+        }
+
     } catch (error) {
-        console.log(error)
-        return error
+        console.warn(error.message)
+        return {
+            error: true,
+            errorDetails: error.message
+        }
     }
 }
