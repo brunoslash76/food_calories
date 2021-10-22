9af4b0ca0fdfcc46a7f2718c39894748dd0e4be6
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-22 16:14:49 -0300

    feat: add Providers for Redux, LocalizationProvider, BrowserRouter and ToastContainer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components'
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DateFnsUtils from '@date-io/date-fns';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 import App from './App';
 import theme from './styles/theme.styles'
 import GlobalStyles from './styles/globalStyles.styles'
+import { store } from './config/redux';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <LocalizationProvider dateAdapter={DateFnsUtils}>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <App />
+            <ToastContainer />
+          </ThemeProvider>
+        </BrowserRouter>
+      </LocalizationProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
