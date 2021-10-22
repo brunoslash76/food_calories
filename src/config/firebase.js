5679ce654d9288d34a3c41238b9ec530ceb22b0c
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-22 15:59:49 -0300

    chore: adjust code organization to firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import env from 'react-dotenv'
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore/lite';
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,8 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
 const auth = getAuth(app)
+const db = getFirestore(app)
 export {
     auth,
     db
