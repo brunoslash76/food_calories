985576d411b726ee7ce3db69029418cf5c472b17
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-22 15:56:52 -0300

    feat: add register http call to firebase

--- src/http/register.js
+++ src/http/register.js
@@ -1,10 +1,26 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { doc, setDoc } from 'firebase/firestore'
+import { localStorageUser } from '../adapters/constants'
+import { localStorageSetItem } from '../adapters/localStorageAdapter'
+import { auth, db } from '../config/firebase'
 
-import { auth } from '../config/firebase'
 
-export const register = (user) => {
+export const register = async (user) => {
     try {
-        const res = createUserWithEmailAndPassword(auth, user.email, user.password)
+        const res = await createUserWithEmailAndPassword(auth, user.email, user.password)
+        const userRef = doc(
+            db,
+            'users/' + res.user.uid,
+        )
+        const userLocal = {
+            username: user.username,
+            email: user.email,
+            uid: res.user.uid,
+            isAdmin: user.isAdmin,
+            accessToken: res.user.accessToken
+        }
+        localStorageSetItem(localStorageUser, userLocal)
+        setDoc(userRef, userLocal)
         return res
     } catch (error) {
         return error
