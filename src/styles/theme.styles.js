2990e2fb5cf44d987ea9ec521fd143aa1c752841
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-22 15:49:11 -0300

    chore: add sets of colos and sizes

--- src/styles/theme.styles.js
+++ src/styles/theme.styles.js
@@ -1,13 +1,29 @@
 const theme = {
     colors: {
         primary: '#439CC2',
+        primaryDarker: '#367b98',
         secundary: '#555555',
         background: '#B7E1EE',
         white: '#fefefe',
         default: '#676767',
         warning: '#ECBD77',
+        warningDarker: '#e0a752',
         danger: '#B86D6D',
-        grey: '#C4C4C4'
+        dangerDarker: '#985a5a',
+        success: '#29AB45',
+        grey: '#C4C4C4',
+        colorsSet: [
+            'primary',
+            'secundary',
+            'background',
+            'white',
+            'default',
+            'warning',
+            'warningDarker',
+            'danger',
+            'grey',
+            'success'
+        ]
     },
     fonts: {
         family: 'Roboto',
@@ -17,13 +33,27 @@ const theme = {
             medium: '1.8rem',
             small: '1.6rem',
             xsmall: '1.4rem',
-            xxsmal: '1.3rem'
+            xxsmal: '1.3rem',
+            sizesSet: [
+                'xxlarge',
+                'xlarge',
+                'medium',
+                'small',
+                'xsmall',
+                'xxsmal',
+            ]
         },
         weight: {
             thin: '100',
             light: '300',
             regular: '400',
-            bold: '700'
+            bold: '700',
+            weightSet: [
+                'thin',
+                'light',
+                'regular',
+                'bold',
+            ]
         }
     },
     navBarHeight: '80px'
