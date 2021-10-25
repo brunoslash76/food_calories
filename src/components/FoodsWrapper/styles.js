a99784501aa2fefa7023c30195ed126fb80d2537
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-24 22:33:58 -0300

    refactor: move code to Modal shared

--- src/components/FoodsWrapper/styles.js
+++ src/components/FoodsWrapper/styles.js
@@ -29,66 +29,7 @@ const AddEntryButton = styled.button`
     `}
 `
 
-const CloseButton = styled.button`
-    ${({ theme }) => css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: ${theme.colors.primary};
-        font-size: ${theme.fonts.sizes.xlarge};
-        font-weight: ${theme.fonts.weight.light};
-        border-radius: 50%;
-        height: 30px;
-        width: 30px;
-        transition: all .3s;
-        cursor: pointer;
-        &:hover {
-            background-color: rgba(180, 180, 180, 0.5);
-        }
-    `}
-`
-
-const FoodEntryContainer = styled.div`
-    display: flex;
-`
-
-const ModalCard = styled.div`
-    ${({ theme }) => css`
-        height: 290px;
-        width: 480px;
-        padding: 16px;
-        background-color: ${theme.colors.white};
-        border-radius: 4px;
-    `}
-`
-const ModalFormContainer = styled.form`
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        height: 100%;
-`
-
-const ModalCardHeader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
-const ModalCardBody = styled.div`
-    padding: 16px 0;
-`
-const ModalCardFooter = styled.div`
-    display: flex;
-    justify-content: flex-end;
-`
-
 export {
     AddEntryButton,
-    CloseButton,
-    FoodEntryContainer,
-    ModalCard,
-    ModalCardHeader,
-    ModalCardBody,
-    ModalCardFooter,
-    ModalFormContainer,
     Wrapper
 }
