eabf4183ee652c599d2bde79cd73488506d8d718
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-24 22:27:00 -0300

    feat: add change daily calories input

--- src/components/FoodsWrapper/components/FoodsList/index.js
+++ src/components/FoodsWrapper/components/FoodsList/index.js
@@ -1,17 +1,42 @@
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
-import { useSelector } from 'react-redux'
+import BlockIcon from '@mui/icons-material/Block';
+import { TextField } from '@mui/material'
 import { Title, Text } from '../../../Typography'
 import FoodItem from '../FoodItem'
 import { Header } from './styles'
+import Button from '../../../Button'
+
 
 const FoodList = ({ foodArray = [] }) => {
     const { caloriesThreshold } = useSelector(state => state.caloriesReducer)
+    const dispatch = useDispatch()
+    const [localFoodArray, setLocalFoodArray] = useState(foodArray)
+    const [showCaloriesInput, setShowCaloriesInput] = useState(false)
+    const [calories, setCalories] = useState(caloriesThreshold)
+
+    useEffect(() => {
+        setLocalFoodArray(foodArray)
+    }, [foodArray])
 
     if (foodArray.length <= 0) return null
 
+    const handleAlterCalories = () => {
+        setShowCaloriesInput(true)
+    }
+
+    const handleSaveCalories = () => {
+        console.log(calories)
+        dispatch({ type: 'SET_NEW_DAILY_CALORIES', payload: calories })
+        setShowCaloriesInput(false)
+    }
 
     const mapFoodArray = () => {
-        const datesAux = foodArray.map(food => food.date)
+        const datesAux = localFoodArray.map(food => {
+            const date = new Date(food.date)
+            return date.toISOString().split('T')[0]
+        })
         const dates = new Set(datesAux)
         const array = []
         dates.forEach(date => {
@@ -19,9 +44,10 @@ const FoodList = ({ foodArray = [] }) => {
                 date,
                 foods: []
             }
-            for (let i = 0; i < foodArray.length; i++) {
-                if (foodArray[i].date === date) {
-                    obj.foods.push(foodArray[i])
+            for (let i = 0; i < localFoodArray.length; i++) {
+                const foodDate = new Date(localFoodArray[i].date)
+                if (foodDate.toISOString().split('T')[0] === date) {
+                    obj.foods.push(localFoodArray[i])
                 }
             }
             array.push(obj)
@@ -39,11 +65,36 @@ const FoodList = ({ foodArray = [] }) => {
         <>
             <Header>
                 <Title>Foods</Title>
-                <Text>
-                    Daily Calories Limit
-                    <strong> {caloriesThreshold} </strong>
-                    kcal
-                </Text>
+                {showCaloriesInput
+                    ? (
+                        <div>
+                            <TextField
+                                type="number"
+                                onChange={event => setCalories(event.target.value)} value={calories}
+                                variant='standard'
+                                style={{ marginRight: '16px' }}
+                            />
+                            <Button
+                                style={{ marginRight: '16px' }}
+                                onClick={handleSaveCalories}
+
+                            >
+                                Save
+                            </Button>
+                            <Button
+                                onClick={() => setShowCaloriesInput(false)}
+                            >
+                                <BlockIcon />
+                            </Button>
+                        </div>
+                    )
+                    : (
+                        <Text onClick={handleAlterCalories}>
+                            Daily Calories Limit
+                            <strong> {calories} </strong>
+                            kcal
+                        </Text>
+                    )}
             </Header>
             {mapFoodArray()}
         </>
@@ -65,3 +116,18 @@ FoodList.propTypes = {
 
 
 export default FoodList
+const a = [
+    '1633907387000',
+    '1633908312000',
+    '1633993805000',
+    '1634080221000',
+    '1634166635000',
+    '1634253051000',
+    '1634339466000',
+    '1634426167000',
+    '1634512518000',
+    '1634698800000',
+    '1634858198000',
+    '1635031337000',
+    '1635117719000',
+]
---
+++ src/components/Modals/index.js
@@ -0,0 +1,7 @@
+import AddFoodEntryModal from "./components/AddFoodEntryModal";
+import ConfirmationModal from "./components/ConfirmationModal";
+
+export {
+    AddFoodEntryModal,
+    ConfirmationModal
+}
