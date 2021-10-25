c21fb9d40e673c5cdf7e1e7da2a93e27a5438dd4
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-24 22:14:47 -0300

    feat: add customQuery to queryHelper

--- src/utils/queryHelper.js
+++ src/utils/queryHelper.js
@@ -2,9 +2,13 @@ import { getDocs, query, collection } from 'firebase/firestore'
 
 import { db } from '../config/firebase'
 
-export const queryHelper = async (collectionName) => {
-    if (!collectionName) return null
-    const q = query(collection(db, collectionName));
+export const queryHelper = async (collectionName, customQuery = null) => {
+    let q;
+    if (collectionName && typeof collectionName === 'string') {
+        q = query(collection(db, collectionName));
+    } else if (customQuery) {
+        q = customQuery
+    }
     const queryUsersSnapshot = await getDocs(q);
     const data = []
     queryUsersSnapshot.forEach(doc => data.push(doc.data()))
