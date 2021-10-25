a6240faf0f14e1f1f41e045e975817ac25425947
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-24 22:22:34 -0300

    feat: add Admin navigation to NavBar compopnent

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,6 +1,8 @@
-import { useDispatch } from 'react-redux'
+import { Link, useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { signOut } from '@firebase/auth'
 import Avatar from '@mui/material/Avatar'
+
 import { auth } from '../../config/firebase'
 import { LOGOFF_USER } from '../../store/constants'
 import Title from '../Typography/Title'
@@ -8,6 +10,8 @@ import { NavContainer } from './styles'
 
 const NavBar = ({ title }) => {
     const dispatch = useDispatch()
+    const { user } = useSelector(state => state.authReducer)
+    const history = useHistory()
     const logoff = () => {
         signOut(auth).then(() => {
             localStorage.clear()
@@ -15,10 +19,26 @@ const NavBar = ({ title }) => {
         })
     }
 
+    const handleNavibarLink = () => {
+        if (user.isAdmin && history.location.pathname === '/admin-home') {
+            return <Link to=''>Home</Link>
+        } else if (!user.isAdmin) {
+            return null
+        } else {
+            return <Link to='/admin-home'>Admin</Link>
+        }
+    }
+
     return (
         <NavContainer>
             <Title color="white">{title}</Title>
-            <Avatar size='large' onClick={logoff} style={{ cursor: 'pointer' }} />
+            {handleNavibarLink()}
+            <Avatar
+                size='large'
+                onClick={logoff}
+                style={{ cursor: 'pointer' }}
+                title={user.username}
+            />
         </NavContainer>
     )
 }
