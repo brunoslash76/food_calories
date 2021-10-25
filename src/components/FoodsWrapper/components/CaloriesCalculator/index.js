49d482f561d39bf6c6210d54f38789a39f0791b3
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-24 22:38:22 -0300

    refactor: remove console.log

--- src/components/FoodsWrapper/components/CaloriesCalculator/index.js
+++ src/components/FoodsWrapper/components/CaloriesCalculator/index.js
@@ -4,7 +4,6 @@ import { CaloriesLimit } from './styles';
 const CaloriesCalculator = ({ foods }) => {
 
     const { caloriesThreshold } = useSelector(state => state.caloriesReducer)
-    console.log(caloriesThreshold)
     let kcalTotal = 0;
     if (foods.foods.length > 0) {
         for (let i = 0; i < foods.foods.length; i++) {
