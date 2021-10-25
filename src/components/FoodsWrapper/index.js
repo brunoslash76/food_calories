b020ae1355057a36527c9bc105b3a9765e8e4d22
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-24 22:34:25 -0300

    refactor: move modal to its own component

--- src/components/FoodsWrapper/index.js
+++ src/components/FoodsWrapper/index.js
@@ -1,26 +1,19 @@
 import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify'
-import { Modal, TextField } from '@mui/material'
-import DatePicker from '@mui/lab/DatePicker';
-import TimePicker from '@mui/lab/TimePicker';
 
 import { SubTitle } from '../Typography'
-import Button from '../Button'
 import FoodList from './components/FoodsList'
 import {
+  addFoodEntry,
+  getFoodEntries
+} from '../../http/foodEntry';
+import {
   AddEntryButton,
-  CloseButton,
-  FoodEntryContainer,
-  ModalCard,
-  ModalCardHeader,
-  ModalCardBody,
-  ModalCardFooter,
-  ModalFormContainer,
   Wrapper,
 } from './styles'
-import { useSelector } from 'react-redux';
-import { addFoodEntry, getFoodEntries } from '../../http/foodEntry';
-import { connectFirestoreEmulator } from '@firebase/firestore';
+import { AddFoodEntryModal } from '../Modals';
+import { FoodEntry } from '../../models/FoodEntry';
 
 const FoodsWrapper = () => {
   const [foodEntries, setFoodEntries] = useState([])
@@ -36,7 +29,7 @@ const FoodsWrapper = () => {
       if (user.uid) {
         try {
           const data = await getFoodEntries(user.uid)
-          setFoodEntries(data)
+          setFoodEntries(data ?? [])
         } catch (error) {
           toast.error('Oops something wrong is not right!')
         }
@@ -52,25 +45,18 @@ const FoodsWrapper = () => {
   const handleSubmit = async (event) => {
     event.preventDefault()
     const { foodName, foodCalories } = event.target
-    const data = {
-      userId: user.uid,
-      foodName: foodName.value,
-      foodCalories: foodCalories.value,
-      date: dateTime.date,
-      time: dateTime.time
-    }
-    clearInputs(event.target)
-    setFoodEntries(state => ([...state, data]))
-    const res = await addFoodEntry(data)
-  }
+    const data = new FoodEntry(
+      foodName.value,
+      foodCalories.value,
+      dateTime.date,
+      dateTime.time,
+      user.uid
+    )
 
-  const clearInputs = (form) => {
-    form.foodName.value = null
-    form.foodCalories.value = null
-    setDateTime({
-      date: null,
-      time: null,
-    })
+
+    setFoodEntries(state => ([...state, data]))
+    await addFoodEntry(data)
+    event.target.reset()
   }
 
   const handleCancel = () => {
@@ -79,19 +65,6 @@ const FoodsWrapper = () => {
       date: null,
       time: null
     })
-  }
-
-  const handleTimeChange = (newValue) => {
-    const date = new Date(newValue)
-
-    const time = date.getTime()
-    setDateTime(state => ({ ...state, time }));
-  }
-
-  const handleDateChange = (newValue) => {
-    const date = new Date(newValue)
-    const formatedDate = date.toISOString().split('T')[0]
-    setDateTime(state => ({ ...state, date: formatedDate }));
   }
 
   return (
@@ -105,92 +78,18 @@ const FoodsWrapper = () => {
         )
         : <FoodList foodArray={foodEntries} />
       }
-      <Modal
+      <AddFoodEntryModal
         open={open}
-        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-      >
-        <ModalCard>
-          <ModalFormContainer onSubmit={handleSubmit}>
-            <ModalCardHeader>
-              <SubTitle color="primary">New Food Entry</SubTitle>
-              <CloseButton onClick={handleCloseModal}>x</CloseButton>
-            </ModalCardHeader>
-            <ModalCardBody>
-              <FoodEntryContainer >
-                <section style={{ paddingRight: '4px' }}>
-                  <TextField
-                    id="foodName"
-                    label="Food Name"
-                    fullWidth
-                    style={{ marginBottom: '24px' }}
-                    variant="standard"
-                    color="primary"
-                  />
-                  <TextField
-                    id="foodCalories"
-                    label="Food Calories"
-                    fullWidth
-                    style={{ marginBottom: '24px' }}
-                    variant="standard"
-                    color="primary"
-                  />
-                </section>
-                <section style={{ paddingLeft: '4px', flex: 1 }}>
-                  <div style={{ marginBottom: '24px' }}>
-                    <DatePicker
-                      style={{ marginBottom: '24px' }}
-                      label="Date"
-                      value={dateTime.date}
-                      onChange={handleDateChange}
-
-                      renderInput={(params) => {
-                        return <TextField
-                          id="date" {...params}
-                          variant="standard"
-                          fullWidth
-                          color="primary"
-                        />
-                      }}
-                      variant="standard"
-                    />
-                  </div>
-                  <TimePicker
-                    style={{ marginBottom: '24px' }}
-                    label="Time"
-                    value={dateTime.time}
-                    onChange={handleTimeChange}
-
-                    renderInput={(params) => {
-                      return <TextField
-                        id="time" {...params}
-                        variant="standard"
-                        fullWidth
-                        color="primary"
-                      />
-                    }}
-                  />
-                </section>
-              </FoodEntryContainer>
-            </ModalCardBody>
-            <ModalCardFooter>
-              <Button
-                type="button"
-                color="danger"
-                withRightSpacing
-                onClick={handleCancel}
-              >
-                cancel
-              </Button>
-              <Button
-                type="submit"
-                color="primary"
-              >
-                submit
-              </Button>
-            </ModalCardFooter>
-          </ModalFormContainer>
-        </ModalCard>
-      </Modal>
+        handleCancel={handleCancel}
+        handleCloseModal={handleCloseModal}
+        handleDateChange={(newValue) => setDateTime(state => ({ ...state, date: newValue }))}
+        handleSubmit={handleSubmit}
+        handleTimeChange={(newValue) => setDateTime(state => ({ ...state, time: newValue }))}
+        state={{
+          ...foodEntries,
+          ...dateTime
+        }}
+      />
       <AddEntryButton onClick={handleAddFoodEntry}>
         +
       </AddEntryButton>
