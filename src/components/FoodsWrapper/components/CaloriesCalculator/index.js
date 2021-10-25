aed54a785373a70440dad176733d2039f9c94b02
Bruno Russo <btuno.russo@adventhealth.com>
2021-10-24 22:33:17 -0300

    refactor: change indentation

--- src/components/FoodsWrapper/components/CaloriesCalculator/index.js
+++ src/components/FoodsWrapper/components/CaloriesCalculator/index.js
@@ -4,15 +4,19 @@ import { CaloriesLimit } from './styles';
 const CaloriesCalculator = ({ foods }) => {
 
     const { caloriesThreshold } = useSelector(state => state.caloriesReducer)
+    console.log(caloriesThreshold)
     let kcalTotal = 0;
     if (foods.foods.length > 0) {
         for (let i = 0; i < foods.foods.length; i++) {
             kcalTotal += Number(foods.foods[i].foodCalories)
-            console.log(Number(foods.foods[i].foodCalories))
         }
     }
     return (
-        <CaloriesLimit error={kcalTotal > caloriesThreshold}>total de calorias: {kcalTotal} kcal</CaloriesLimit>
+        <CaloriesLimit
+            error={kcalTotal > caloriesThreshold}
+        >
+            total de calorias: {kcalTotal} kcal
+        </CaloriesLimit>
     )
 }
 
